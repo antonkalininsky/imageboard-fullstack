2646688ecd222ede36aad8756c555acc6135407a
Anton <chadik.free@gmail.com>
2023-12-01 14:09:26 +0600

    cleaning up

--- frontend/src/components/UI/ThreadContainer.js
+++ frontend/src/components/UI/ThreadContainer.js
@@ -4,23 +4,19 @@ import { mdiChat, mdiBookmark } from '@mdi/js';
 
 export default function ThreadContainer(props) {
 
-  const test = () => {
-    console.log('hellow world');
-  }
-
   return (
     <div className='container text-white bg-gray p-8 max-w-xl mb-6 rounded-xl shadow-main'>
-        <h3 className='text-2xl font-semibold mb-5'>{props.header}</h3>
-        {/* <div className='flex mb-5'>
+      <h3 className='text-2xl font-semibold mb-5'>{props.header}</h3>
+      {/* <div className='flex mb-5'>
             <div>file1</div>
         </div> */}
-        <div className='mb-5 text-lg font-medium'>
+      <div className='mb-5 text-lg font-medium'>
         {props.text}
-        </div>
-        <div className='flex space-x-2'>
-          <MyButton icon={mdiBookmark} counter={false} onClick={test} />
-          <MyButton text={'Ответить'} icon={mdiChat} counter={props.counter} />
-        </div>
+      </div>
+      <div className='flex space-x-2'>
+        <MyButton icon={mdiBookmark} counter={false} onClick={props.onEnter} />
+        <MyButton text={'Ответить'} icon={mdiChat} counter={props.counter} onClick={props.addFavourite} />
+      </div>
     </div>
   )
 }
--- frontend/src/views/Threads.js
+++ frontend/src/views/Threads.js
@@ -3,9 +3,6 @@ import axios from 'axios'
 import ThreadBlock from '../components/ThreadBlock'
 import ThreadForm from '../components/ThreadForm'
 import ThreadContainer from '../components/UI/ThreadContainer'
-import PinkButton from '../components/UI/PinkButton'
-import MyInput from '../components/UI/MyInput'
-import MyTextArea from '../components/UI/MyTextArea'
 
 export default function Threads() {
 
@@ -18,30 +15,17 @@ export default function Threads() {
   }
 
   useEffect(() => {
-    // fetchData()
+    fetchData()
   }, [])
 
   useEffect(() => {
-    setPostList(posts.map(post => <ThreadBlock {...post} key={post.id} />))
+    setPostList(posts.map(post => <ThreadContainer {...post} key={post.id} />))
   }, [JSON.stringify(posts)])
 
-  const submitPost = async (value) => {
-    await axios.post('/threads', {
-      text: value
-    })
-    await fetchData()
-  }
-
   return (
-    <div className='bg-gray-dark flex flex-col justify-start items-center pt-10'>
-      {/* {postList}
-      <ThreadForm submit={submitPost} /> */}
-      <MyInput className="w-80 mb-2 mt-5" />
-      <MyTextArea className="w-80 mb-2" />
-      <PinkButton className="w-80 mb-8">Add Thread</PinkButton>
-      <ThreadContainer header={'test1'} text={'test test test'} counter={5} />
-      <ThreadContainer header={'test2'} text={'test test test'} counter={0} />
-      <ThreadContainer header={'test3'} text={'test test test'} counter={500} />
+    <div className='flex flex-col justify-start items-center pt-10'>
+      <ThreadForm triggerUpdate={fetchData} />
+      {postList}
     </div>
   );
 }
