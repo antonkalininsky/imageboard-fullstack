be8651b968bbea25602c0712ce573f0e781af7e9
Anton <chadik.free@gmail.com>
2023-12-21 18:22:08 +0600

    front: post form text stylization buttons

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import PinkButton from './UI/PinkButton'
 import Checkbox from './UI/Checkbox'
 import MyInput from './UI/MyInput'
+import MyButton from './UI/MyButton'
 
 const defaultForm = {
     title: '',
@@ -11,11 +12,34 @@ const defaultForm = {
 }
 
 export default function PostForm(props) {
-    const [form, setForm] = useState({...defaultForm})
+    const [form, setForm] = useState({ ...defaultForm })
+    const [selected, setSelected] = useState({
+        start: 0,
+        end: 0
+    })
 
     const handleSubmitPost = () => {
         props.submit(form)
-        setForm({...defaultForm})
+        setForm({ ...defaultForm })
+    }
+
+    const handleTest = (styler) => () => {
+        if (selected.start === selected.end) return
+        const part1 = form.content.slice(0, selected.start)
+        const part2 = form.content.slice(selected.start, selected.end)
+        const part3 = form.content.slice(selected.end)
+        setForm({
+            ...form,
+            content: part1 + styler + part2 + styler + part3
+        })
+    }
+
+    const handleMouseUp = () => {
+        const txtarea = document.getElementById("post-form-input")
+        setSelected({
+            start: txtarea.selectionStart,
+            end: txtarea.selectionEnd
+        })
     }
 
     return (
@@ -32,11 +56,16 @@ export default function PostForm(props) {
                 className='block resize-none bg-gray-dark border-pink border-2 text-white focus:outline-none mb-3 p-2 rounded-lg'
                 value={form.content}
                 onChange={(e) => setForm({ ...form, content: e.target.value })}
+                onMouseUp={handleMouseUp}
             />
             <div className='flex justify-between'>
                 <div>
                     <Checkbox updateValue={(value) => setForm({ ...form, sage: value })}>Sage</Checkbox>
-                    <Checkbox updateValue={(value) => setForm({ ...form, isOp: value})}>OP</Checkbox>
+                    <Checkbox updateValue={(value) => setForm({ ...form, isOp: value })}>OP</Checkbox>
+                </div>
+                <div className='flex'>
+                    <MyButton className='mr-1' text={'bold'} onClick={handleTest('*')} />
+                    <MyButton text={'italic'} onClick={handleTest('**')} />
                 </div>
                 <PinkButton className="w-min" onClick={handleSubmitPost}>Send</PinkButton>
             </div>
